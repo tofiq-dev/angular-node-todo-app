3fc4dc776316fa54dcae38608025f534acad9ed5
tofiq-dev <tofir0303@gmail.com>
2025-02-06 20:55:09 +0500

    Create todoModel.js

---
+++ backend/models/todoModel.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+    text: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
